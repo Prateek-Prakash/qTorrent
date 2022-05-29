175097dd13bfaee19d7d4a924ca6c9c3b1051775
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2022-05-29 14:08:43 -0400

    Mock Log Filter Menu

--- qTorrent/View/LogsPrefsView.swift
+++ qTorrent/View/LogsPrefsView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct LogsPrefsView: View {
+    @State var displayNormal = true
+    @State var displayInfo = true
+    @State var displayWarning = true
+    @State var displayCritical = true
+    
     var body: some View {
         VStack {
             List {
@@ -16,5 +21,46 @@ struct LogsPrefsView: View {
         }
         .navigationTitle("Logs")
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            Menu {
+                Button {
+                    displayNormal.toggle()
+                } label: {
+                    Text("Normal").tag(1)
+                    if displayNormal {
+                        Image(systemName: "checkmark")
+                    }
+                }
+                
+                Button {
+                    displayInfo.toggle()
+                } label: {
+                    Text("Info").tag(2)
+                    if displayInfo {
+                        Image(systemName: "checkmark")
+                    }
+                }
+                
+                Button {
+                    displayWarning.toggle()
+                } label: {
+                    Text("Warning").tag(3)
+                    if displayWarning {
+                        Image(systemName: "checkmark")
+                    }
+                }
+                
+                Button {
+                    displayCritical.toggle()
+                } label: {
+                    Text("Critical").tag(8)
+                    if displayCritical {
+                        Image(systemName: "checkmark")
+                    }
+                }
+            } label: {
+                Label("Filter", systemImage: "slider.horizontal.3")
+            }
+        }
     }
 }
