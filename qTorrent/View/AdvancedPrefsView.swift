6be7d9aa2968a072b932d2289db9b813d2de4c39
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2022-05-29 01:40:52 -0400

    Configuration Mocking

--- qTorrent/View/AdvancedPrefsView.swift
+++ qTorrent/View/AdvancedPrefsView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct AdvancedPrefsView: View {
+    @State var recheckCompletedTorrents = false
+    @State var resolvePeerCountrie = true
+    @State var embeddedTracker = false
+    @State var enableOSCache = true
+    @State var coalesceReadsWrites = true
+    
     var body: some View {
         VStack {
             List {
+                Section(header: Text("QBITTORRENT SECTION")) {
+                    Toggle(isOn: $recheckCompletedTorrents) {
+                        Text("Recheck Completed Torrents")
+                    }
+                    
+                    Toggle(isOn: $resolvePeerCountrie) {
+                        Text("Resolve Peer Countries")
+                    }
+                    
+                    Toggle(isOn: $embeddedTracker) {
+                        Text("Embedded Tracker")
+                    }
+                    
+                    Text("Embedded Tracker Port").badge("9000")
+                }
                 
+                Section(header: Text("LIBTORRENT SECTION")) {
+                    Text("Asynchronous IO Threads").badge("10")
+                    
+                    Text("Hashing Threads").badge("1")
+                    
+                    Text("File Pool Size").badge("5000")
+                    
+                    Toggle(isOn: $enableOSCache) {
+                        Text("Enable OS Cache")
+                    }
+                    
+                    Toggle(isOn: $coalesceReadsWrites) {
+                        Text("Coalesce Reads & Writes")
+                    }
+                }
             }
         }
         .navigationTitle("Advanced")
