bd6708b3e19822da61774789794a1d842f6c324e
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2022-05-30 08:22:32 -0400

    Working Logs Filter

--- qTorrent/View/LogsPrefsView.swift
+++ qTorrent/View/LogsPrefsView.swift
@@ -11,10 +11,7 @@ struct LogsPrefsView: View {
     @State var completeList: [MainLog]? = []
     @State var filteredList: [MainLog]? = []
     
-    @State var displayNormal = true
-    @State var displayInfo = true
-    @State var displayWarning = true
-    @State var displayCritical = true
+    @State var displayeTypes = [ 1, 2, 3, 8 ]
     
     let refreshTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
@@ -25,22 +22,25 @@ struct LogsPrefsView: View {
                     if let logs = filteredList {
                         ForEach(logs.indices, id: \.self) { logIndex in
                             let log = logs[logIndex]
-                            HStack(alignment: .center, spacing: 0) {
-                                VStack(alignment: .leading, spacing: 3) {
-                                    Text(String(log.getLogLevel()))
-                                        .font(.system(size: 10, weight: .thin))
-                                        .foregroundColor(log.getLogColor())
-                                    
-                                    Text(log.message!.uppercased())
-                                        .font(.system(size: 10, weight: .bold))
-                                    
-                                    Text(String(log.getDate()))
-                                        .font(.system(size: 10, weight: .bold))
-                                        .foregroundColor(.secondary)
+                            if displayeTypes.contains(log.type!) {
+                                HStack(alignment: .center, spacing: 0) {
+                                    VStack(alignment: .leading, spacing: 3) {
+                                        Text(String(log.getLogLevel()))
+                                            .font(.system(size: 10, weight: .thin))
+                                            .foregroundColor(log.getLogColor())
+                                        
+                                        Text(log.message!.uppercased())
+                                            .font(.system(size: 10, weight: .bold))
+                                        
+                                        Text(String(log.getDate()))
+                                            .font(.system(size: 10, weight: .bold))
+                                            .foregroundColor(.secondary)
+                                    }
+                                    .padding(.horizontal)
                                 }
-                                .padding(.horizontal)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                
                             }
-                            .frame(maxWidth: .infinity, alignment: .leading)
                         }
                     }
                 }
@@ -53,37 +53,53 @@ struct LogsPrefsView: View {
         .toolbar {
             Menu {
                 Button {
-                    displayNormal.toggle()
+                    if displayeTypes.contains(1) {
+                        displayeTypes.remove(at: displayeTypes.firstIndex(of: 1)!)
+                    } else {
+                        displayeTypes.append(1)
+                    }
                 } label: {
                     Text("Normal").tag(1)
-                    if displayNormal {
+                    if displayeTypes.contains(1) {
                         Image(systemName: "checkmark")
                     }
                 }
                 
                 Button {
-                    displayInfo.toggle()
+                    if displayeTypes.contains(2) {
+                        displayeTypes.remove(at: displayeTypes.firstIndex(of: 2)!)
+                    } else {
+                        displayeTypes.append(2)
+                    }
                 } label: {
                     Text("Info").tag(2)
-                    if displayInfo {
+                    if displayeTypes.contains(2) {
                         Image(systemName: "checkmark")
                     }
                 }
                 
                 Button {
-                    displayWarning.toggle()
+                    if displayeTypes.contains(3) {
+                        displayeTypes.remove(at: displayeTypes.firstIndex(of: 3)!)
+                    } else {
+                        displayeTypes.append(3)
+                    }
                 } label: {
                     Text("Warning").tag(3)
-                    if displayWarning {
+                    if displayeTypes.contains(3) {
                         Image(systemName: "checkmark")
                     }
                 }
                 
                 Button {
-                    displayCritical.toggle()
+                    if displayeTypes.contains(8) {
+                        displayeTypes.remove(at: displayeTypes.firstIndex(of: 8)!)
+                    } else {
+                        displayeTypes.append(8)
+                    }
                 } label: {
                     Text("Critical").tag(8)
-                    if displayCritical {
+                    if displayeTypes.contains(8) {
                         Image(systemName: "checkmark")
                     }
                 }
